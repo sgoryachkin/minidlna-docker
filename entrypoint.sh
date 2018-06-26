e347751da574d92dc4bf7b91bbffef61b25b5689
sgoryachkin <artful.tiger@gmail.com>
2018-06-27 00:15:19 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ for VAR in $(env); do
   if [[ "$VAR" =~ ^MINIDLNA_ ]]; then
     minidlna_name=$(echo "$VAR" | sed -r "s/MINIDLNA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]')
     minidlna_value=$(echo "$VAR" | sed -r "s/.*=(.*)/\1/g")
+    echo "${minidlna_name}=${minidlna_value}"
     echo "${minidlna_name}=${minidlna_value}" >> /etc/minidlna.conf
   fi
 done
